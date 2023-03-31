48d38b0bc7c2f9546aa0ced0f5dbe303053d5b9b
Antoan Sarafski <98612405+Borntohate@users.noreply.github.com>
2023-03-31 17:26:13 +0300

    Commit with tested Constructor of TextBook.

--- UniversityLibrary.Test/UnitTest1.cs
+++ UniversityLibrary.Test/UnitTest1.cs
@@ -3,15 +3,24 @@ namespace UniversityLibrary.Test
     using NUnit.Framework;
     public class Tests
     {
+        private TextBook textBook;
+        private string title = "1984";
+        private string author = "George Orwell";
+        private string category = "Disthopy";
+
+
         [SetUp]
         public void Setup()
         {
+            textBook = new TextBook(title, author, category);
         }
 
         [Test]
-        public void Test1()
+        public void TestTextbookConstructor_SetsValuesCorrectly()
         {
-            Assert.Pass();
+            Assert.AreEqual(textBook.Category, category);
+            Assert.AreEqual(textBook.Author, author);
+            Assert.AreEqual(textBook.Title, title);
         }
     }
 }
